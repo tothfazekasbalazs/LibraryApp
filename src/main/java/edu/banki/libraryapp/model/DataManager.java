5353a88b9cd84b4bef9bff257c670d58a4aee477
Tóth Fazekas Balázs <toth-fazekas.balazs.2021i@bankitatabanya.hu>
2025-10-09 13:08:59 +0200

    Done

--- src/main/java/edu/banki/libraryapp/model/DataManager.java
+++ src/main/java/edu/banki/libraryapp/model/DataManager.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class DataManager {
     private static final List<Book> books = new ArrayList<>();
@@ -12,14 +13,32 @@ public class DataManager {
         books.add(book);
     }
 
+//    public static Map<String, Long> getStock() {
+//        Map<String, Long> stock = new HashMap<>();
+//
+//        for (Book book : books) {
+//            stock.put(book.getCategory(), stock.getOrDefault(book.getCategory(), 0L) + 1);
+//        }
+//
+//        return stock;
+//    }
+
+//    public static Map<String, Long> getStock() {
+//        return books.stream().collect(Collectors.groupingBy(Book::getCategory, Collectors.counting()));
+//    }
+
     public static Map<String, Long> getStock() {
         Map<String, Long> stock = new HashMap<>();
-
         for (Book book : books) {
-            stock.put(book.getCategory(), stock.getOrDefault(book.getCategory(), 0L) + 1);
+            String category = book.getCategory();
+            if (stock.containsKey(category)) {
+                Long currentCount = stock.get(category);
+                stock.put(category, currentCount + 1);
+            } else {
+                stock.put(category, 1L);
+           }
         }
-
-        return stock;
+       return stock;
     }
 
 }
