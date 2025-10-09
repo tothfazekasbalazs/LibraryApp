6687b0754cd256185c877fb19690650db921d2f3
Tóth Fazekas Balázs <toth-fazekas.balazs.2021i@bankitatabanya.hu>
2025-10-09 12:14:47 +0200

    UnitTeszt getstock

---
+++ src/test/java/edu/banki/libraryapp/model/DataManagerTest.java
@@ -0,0 +1,40 @@
+package edu.banki.libraryapp.model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.Map;
+
+public class DataManagerTest {
+
+    @Test
+    void testStockMultipleCategory() {
+        //Arrange
+
+        Book book1 = new Book("Author1","Title1","Fiction",2001, LocalDate.now());
+        Book book2 = new Book("Author3","Title2","Szép Irodalom",2002, LocalDate.now());
+        Book book3 = new Book("Author3","Title3","History",2003, LocalDate.now());
+        Book book4 = new Book("Author4","Title4","Fiction4",2004, LocalDate.now());
+        Book book5 = new Book("Author5","Title5","Fiction5",2005, LocalDate.now());
+
+        DataManager.addBook(book1);
+        DataManager.addBook(book2);
+        DataManager.addBook(book3);
+        DataManager.addBook(book4);
+        DataManager.addBook(book5);
+
+        //ACT
+
+        Map<String, Long> stock = DataManager.getStock();
+
+        //ASSERT
+
+        Assertions.assertEquals(1L,stock.get("Fiction"),"1");
+        Assertions.assertEquals(1L,stock.get("Szép Irodalom"),"2");
+        Assertions.assertEquals(1L,stock.get("History"),"3");
+        Assertions.assertEquals(5, stock.size(),"4");
+
+    }
+
+}
