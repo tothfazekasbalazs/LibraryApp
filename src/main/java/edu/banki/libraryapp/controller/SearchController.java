558e6510c2c3b38322e1e401ddc1f17bb9f9aa1d
Tóth Fazekas Balázs <toth-fazekas.balazs.2021i@bankitatabanya.hu>
2025-10-09 13:55:14 +0200

    SearchController initalize

--- src/main/java/edu/banki/libraryapp/controller/SearchController.java
+++ src/main/java/edu/banki/libraryapp/controller/SearchController.java
@@ -1,4 +1,45 @@
 package edu.banki.libraryapp.controller;
 
-public class SearchController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SearchController implements Initializable {
+
+    @FXML
+    TextField searchField;
+
+    @FXML
+    Button seachButton;
+
+    @FXML
+    VBox dataContainer;
+
+    @FXML
+    TableView tableView;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        initTable();
+    }
+
+    private void initTable() {
+        tableView = new TableView<>();
+        TableColumn author = new TableColumn<>("Szerző: ");
+        TableColumn title = new TableColumn<>("Cím: ");
+        TableColumn category = new TableColumn<>("Kategória: ");
+        TableColumn year = new TableColumn<>("Kiadás:  ");
+        TableColumn acquisitionDate = new TableColumn<>("Beszerzési dátum: ");
+
+        tableView.getColumns().addAll(author,title,category, year, acquisitionDate);
+        dataContainer.getChildren().add(tableView);
+    }
+
 }
